d415d475c2ebaa8164b9e195b7f11fab150c1073
Lucas-Porras <abdhalalucas@gmail.com>
2023-03-03 22:42:22 -0300

    Primer avance

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Organizador</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/png" href="favicon.png">
+    <!-- Ion-Icon -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!-- Fuentes -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,900&display=swap" rel="stylesheet">
+    <!-- CSS y JavaScript de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>    
+</head>
+<body>
+    <!-- Aca viene el header y el nav -->
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Organization</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                          <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link disabled">Disabled</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Iniciar Sesion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Registrarse</a>
+                        </li>
+                    </ul>
+                    <!-- Esto vendria a ser el form para buscar y eso -->
+                    <!--<form class="d-flex" role="search">
+                      <input class="form-control me-2" type="search" placeholder="What are you looking for?" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit"><ion-icon name="search-outline"></ion-icon></button>
+                    </form>-->
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div>
+        <!-- Aca viene el div de la bienvenida -->
+        <section class="p-4 mt-4 container d-flex align-items-center">
+            <div class="texto">
+                <h1 class="fw-light">Welcome</h1>
+                <h4 class="fw-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem vero animi fuga nulla nesciunt earum exercitationem? Iusto dolores obcaecati magnam tempora soluta. Neque amet consectetur tenetur molestias doloribus, beatae ipsum.</h4>
+            </div>
+            <div class="index-img">
+                <img src="images/indeximg.jpg" alt="" class="img-fluid">
+            </div>
+        </section>
+    </div>
+        <!-- Aca viene el footer de la pagina-->
+    <footer>
+        <div>
+            <div class="container">
+                <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+                  <p class="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+                  <ul class="nav col-md-4 justify-content-end">
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+                  </ul>
+                </footer>
+              </div>
+        </div>
+    </footer>
+    <style>
+        *{
+            font-family: 'Noto Sans', sans-serif;
+        }
+    </style>
+</body>
+</html>
